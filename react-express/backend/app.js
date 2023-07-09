3987405274aa9fc73911a484955ececfcea91f2f
jasong <jasong@student.42seoul.kr>
2023-07-09 22:40:20 +0900

    chore: sql and dovenv packaged added

--- react-express/backend/app.js
+++ react-express/backend/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
+const fe_dir_path = path.join(__dirname, '../frontend/build');
+const dotenv = require('dotenv');
+
+app.use(express.static(fe_dir_path));
+
+// give static index.html file if exists or throw 404 error
 app.get('/', (req, res) => {
-  res.send('Hello jasong!');
+  app.sendFile(path.join(fe_dir_path, 'index.html'), err => {
+    if (err) {
+      res.status(404).send('Not found');
+    }
+  });
 });
 
 app.listen(8080, () => {
